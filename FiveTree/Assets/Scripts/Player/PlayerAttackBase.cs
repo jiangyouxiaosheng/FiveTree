b8923dea8e55ad41fa1cb1228b688f8290f61485
JiangYou <1758147308@qq.com>
2024-05-09 17:13:47 +0800

    酱油
    技能基类

--- FiveTree/Assets/Scripts/Player/PlayerAttackBase.cs
+++ FiveTree/Assets/Scripts/Player/PlayerAttackBase.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttackBase 
 {
+    [Header("技能基础数据")]
+    private PlayerAttackEnum attackEnum = PlayerAttackEnum.None;//类型
+    private string skillName = "";//名称
+    private int skillDamege = 0;//伤害
+    private float skillCD = 0;//技能CD
+    private float skillNeedConsume = 0;//消耗
+    private float skillNeedTime = 0;//咏唱所需时间
+    private bool isNeedTarget;//是否为指定目标
+    private string skillForTarget = "";//目标
+
+
+  
+
+
+
+
+    [Header("技能UI相关")]
+    [SerializeField]
+    private Image skillImage;//技能图标
+    [SerializeField]
+    private string skillIntroduce="";//技能简介
+
 
 
 }
--- FiveTree/Assets/Scripts/Player/PlayerEnum.cs
+++ FiveTree/Assets/Scripts/Player/PlayerEnum.cs
@@ -1,5 +1,9 @@
+/// <summary>
+/// Íæ¼Ò¹¥»÷ÀàÐÍ
+/// </summary>
 enum PlayerAttackEnum
 {
+    None,
     NormalAttack,
     AttackSkill,
     BuffSkill,
